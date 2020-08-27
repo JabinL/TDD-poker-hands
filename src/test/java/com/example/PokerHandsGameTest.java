120888f111943dd21727e88626f74a0a4c4a617b
JabinL <2099008974@qq.com>
2020-08-27 19:01:04 +0800

    [Pokerhands][Jay,Jabin] feature:pass should_return_white_win_when_run_game_given_2H_2D_5S_9C_KD_3C_4H_5S_6C_7H

--- src/test/java/com/example/PokerHandsGameTest.java
+++ src/test/java/com/example/PokerHandsGameTest.java
@@ -29,8 +29,15 @@ public class PokerHandsGameTest {
 
     @Test
     public void should_return_black_win_when_run_game_given_2H_2D_2S_2C_KD_2C_3H_4S_8C_AH() {
-        String input = "Black: 2H 2D 5S 9C KD White: 2C 3H 4S 8C AH";
+        String input = "Black: 2H 2D 2S 2C KD White: 2C 3H 4S 8C AH";
         String result = pokerHandsGame.run(input);
         assertEquals("Black win",result);
+    }
+
+    @Test
+    public void should_return_white_win_when_run_game_given_2H_2D_5S_9C_KD_3C_4H_5S_6C_7H() {
+        String input = "Black: 2H 2D 5S 9C KD White: 3C 4H 5S 6C 7H";
+        String result = pokerHandsGame.run(input);
+        assertEquals("White win",result);
     }
 }
